6a64bac419bc747e763db62fd8252a701cd03c14
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-10 10:18:09 +0100

    Outputting Dynamic Values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const names = ["Maximilian", "Manuel"];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Aman</p>
+      <p>{chosenName}</p>
       <p>React.js is awesome!</p>
     </div>
   );
