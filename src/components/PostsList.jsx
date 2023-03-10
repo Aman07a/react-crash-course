368fb58fdc99dcffed8d152e7ef6d5e6c5218d9c
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-10 11:52:50 +0100

    Preparing the App For State Management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
-function PostList() {
+function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manuel" body="Check out the full course!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
   );
 }
 
-export default PostList;
+export default PostsList;
